ff3da226011fd56c7fe8cf6d298b0ddbb224532f
RishikesavanRamesh <rishikesavanramesh@proton.me>
2024-10-19 10:32:37 +0530

    Update all

---
+++ ipage/src/components/ContactForm.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+
+const ContactForm = () => {
+    useEffect(() => {
+        // Load the Ratufa script on component mount
+        const script = document.createElement('script');
+        script.id = 'ratufa_loader';
+        script.src = 'https://www.ratufa.io/c/ld.js?f=ou23181v&n=n1.ratufa.io';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            // Cleanup: Remove the script when the component unmounts
+            document.body.removeChild(script);
+        };
+    }, []);
+
+
+
+    return (
+
+        <section id="contact" className="p-5 text-center border w-full max-w-md mx-auto">
+
+
+            <form method="contactForm" className="flex flex-col items-center p-2">
+                <h2 className="text-3xl">Contact Me</h2>
+                <p className="mt-4">Feel free to reach out!</p>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Your Name"
+                    className="border text-white bg-black p-2 m-2 mt-4 w-full max-w-md"
+                    required
+                />
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Your Email"
+                    className="border text-white bg-black p-2 m-2 w-full max-w-md"
+                    required
+                />
+                <textarea
+                    id="message"
+                    name="message"
+                    placeholder="Your Message"
+                    className="border bg-black p-2 m-2 w-full max-w-md h-32"
+                    required
+                />
+                <button
+                    type="submit"
+                    className="border-2 bg-blue-300/50 font-bold hover:bg-blue-300/90 text-blue-300 text-white hover:text-black p-2 mt-4 w-full max-w-md"
+                >
+                    Send
+                </button>
+            </form>
+
+        </section>
+    );
+};
+
+export default ContactForm;
